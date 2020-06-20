0a123af8a5a84daf00c93bebd7e101d9f5c83035
Artem Kuskis <artem.kuskis@gmail.com>
2020-06-20 12:27:17 +0300

    Add pedantic flags and formatter

--- app/src/main/cpp/core/main.cpp
+++ app/src/main/cpp/core/main.cpp
@@ -2,8 +2,8 @@
 #include <string>
 
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_throttle_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */)
+extern "C" JNIEXPORT jstring JNICALL Java_com_example_throttle_MainActivity_stringFromJNI(JNIEnv* env,
+                                                                                          jobject /* this */)
 {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
